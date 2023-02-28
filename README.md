8554c4b436c9a2368523ae4d5f9dcfff98a87f73
mwburke <matthew.wesley.burke@gmail.com>
2023-02-28 13:19:18 -0800

    first pass passenger class and tests

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # RLevator
-Open AI gymnasium environment for elevator control
+
+Open AI gymnasium environment for elevator control. This is currently **WIP** and the 1.0 release will be the first official version considered ready for use.
+
+
---
+++ rlevator/tests/test_passenger.py
@@ -0,0 +1,111 @@
+from rlevator.passenger import Passenger
+
+from copy import deepcopy
+
+
+# Create test passengers
+PASSENGER_0 = Passenger(
+    start_step=0,
+    start_floor=0,
+    destination_floor=5,
+    max_wait_steps=50
+)
+
+def test_reached_destination_negative():
+    assert PASSENGER_0.reached_destination(1) == False
+
+
+def test_reached_destination_positive():
+    assert PASSENGER_0.reached_destination(5) == True
+
+
+def test_age():
+    passenger = deepcopy(PASSENGER_0)
+
+    in_elevator = False
+
+    num_steps = 51
+
+    for _ in range(num_steps):
+        passenger.increment_step(in_elevator)
+
+    assert passenger.get_age() == 51
+
+
+def test_wait():
+    passenger = deepcopy(PASSENGER_0)
+
+    in_elevator = False
+
+    num_steps = 51
+
+    for _ in range(num_steps):
+        passenger.increment_step(in_elevator)
+
+    assert passenger.get_wait() == 51
+
+
+def test_wait_elevator_no_increment():
+    passenger = deepcopy(PASSENGER_0)
+
+    in_elevator = False
+
+    num_steps = 20
+
+    for _ in range(num_steps):
+        passenger.increment_step(in_elevator)
+
+    in_elevator = True
+
+    num_steps = 20
+
+    for _ in range(num_steps):
+        passenger.increment_step(in_elevator)
+
+    assert passenger.get_wait() == 20
+
+
+def test_reached_max_wait_negative():
+    passenger = deepcopy(PASSENGER_0)
+
+    in_elevator = False
+
+    num_steps = 49
+
+    for _ in range(num_steps):
+        passenger.increment_step(in_elevator)
+
+    assert passenger.reached_max_wait() == False
+
+
+def test_reached_max_wait_positive():
+    passenger = deepcopy(PASSENGER_0)
+
+    in_elevator = False
+
+    num_steps = 51
+
+    for _ in range(num_steps):
+        passenger.increment_step(in_elevator)
+
+    assert passenger.reached_max_wait() == True
+
+
+def test_moved_correct_direction_no_move():
+    assert PASSENGER_0.moved_correct_direction(1, 1) == False
+
+
+def test_moved_correct_direction_negative():
+    assert PASSENGER_0.moved_correct_direction(2, 1) == False
+
+
+def test_moved_correct_direction_positive():
+    assert PASSENGER_0.moved_correct_direction(1, 2) == True
+
+
+def test_moved_correct_direction_above_negative():
+    assert PASSENGER_0.moved_correct_direction(6, 7) == False
+
+
+def test_moved_correct_direction_above_positive():
+    assert PASSENGER_0.moved_correct_direction(7, 6) == True
