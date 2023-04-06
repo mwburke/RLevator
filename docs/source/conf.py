c6d42f945c03fec086e633ab8cc8aa4956dfa531
Matthew Burke <matthew.wesley.burke@gmail.com>
2023-04-06 08:30:50 -0700

    Documentation first pass (#9)
    * initial sphinx testing and code autodocumentation
    * environment documentation
    * sphinx build workflow test

---
+++ docs/source/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'RLevator'
+copyright = '2023, Matthew Burke'
+author = 'Matthew Burke'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'myst_parser'
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build']
+
+autoclass_content = 'both'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/source/rlevator.rst
@@ -0,0 +1,50 @@
+rlevator package
+================
+
+rlevator.actions module
+-----------------------
+
+.. automodule:: rlevator.actions
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+rlevator.arrivals module
+------------------------
+
+.. automodule:: rlevator.arrivals
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+rlevator.building module
+------------------------
+
+.. automodule:: rlevator.building
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+rlevator.elevator module
+------------------------
+
+.. automodule:: rlevator.elevator
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+rlevator.environment module
+---------------------------
+
+.. automodule:: rlevator.environment
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+rlevator.passenger module
+-------------------------
+
+.. automodule:: rlevator.passenger
+   :members:
+   :undoc-members:
+   :show-inheritance:
