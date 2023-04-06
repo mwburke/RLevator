014ec3fa800ad2e6a260852a5e691a303121986f
Matthew Burke <mwburke@berkeley.edu>
2023-04-06 10:25:49 -0700

    installing package before auto generating docs, static path back to _static

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,4 +33,4 @@ autoclass_content = 'both'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
-html_static_path = ['static']
+html_static_path = ['_static']
